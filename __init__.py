8da47c04db340ef6c2d58e71aec0e0cb8f9cc0dc
Derek Enos <derek.enos@gmail.com>
2026-04-30 12:51:21 -0400

    chore: reformat using black because don't be an animal

--- __init__.py
+++ __init__.py
@@ -5,16 +5,19 @@
 # Constants
 ###############################################################################
 
-DOCTYPE = '<!DOCTYPE html>\n'
+DOCTYPE = "<!DOCTYPE html>\n"
+
 
 class Entities:
-    DOUBLE_QUOTE = '&quot;'
-    GREATER_THAN = '&gt;'
-    LESS_THAN = '&lt;'
+    DOUBLE_QUOTE = "&quot;"
+    GREATER_THAN = "&gt;"
+    LESS_THAN = "&lt;"
+
 
 ###############################################################################
 # HTMLElement Base Class
 ###############################################################################
+
 
 class HTMLElement:
     # Specify whether this is a void-type element which is self-closing and
@@ -37,21 +40,22 @@ class HTMLElement:
 
     def __init__(self, text=None, children=None, **attrs):
         # Assert that TAG_NAME is defined.
-        if not hasattr(self, 'TAG_NAME'):
-            raise AssertionError('Please subclass HTMLElement and define a '
-                                 'TAG_NAME attribute')
+        if not hasattr(self, "TAG_NAME"):
+            raise AssertionError(
+                "Please subclass HTMLElement and define a " "TAG_NAME attribute"
+            )
         # Assert that text and children are None for a void-type element.
         if self.IS_VOID and (text or children is not None):
             raise AssertionError(
-                'text and children are prohibited for void tag "{}"'
-                .format(self.TAG_NAME)
+                'text and children are prohibited for void tag "{}"'.format(
+                    self.TAG_NAME
+                )
             )
         # Assert that all required attributes were specified.
         if any(k not in attrs for k in self.REQUIRED_ATTRS):
             raise AssertionError(
                 'missing required attrs {} for tag "{}"'.format(
-                    [k for k in self.REQUIRED_ATTRS if k not in attrs],
-                    self.TAG_NAME
+                    [k for k in self.REQUIRED_ATTRS if k not in attrs], self.TAG_NAME
                 )
             )
         # Save the text content value.
@@ -74,13 +78,13 @@ class HTMLElement:
         i.e. func(**{'class': '...', 'data-count': 1}), but that's a lot of
         extra characters.
         """
-        escaped = k[0] == '_'
+        escaped = k[0] == "_"
         i = 1 if escaped else 0
         k_len = len(k)
         while i < k_len:
             c = k[i]
-            if c == '_' and escaped:
-                yield '-'
+            if c == "_" and escaped:
+                yield "-"
             else:
                 yield c
             i += 1
@@ -104,19 +108,18 @@ class HTMLElement:
         HTML entity.
         """
         for c in text:
-            if c == '<':
+            if c == "<":
                 yield from Entities.LESS_THAN
-            elif c == '>':
+            elif c == ">":
                 yield from Entities.GREATER_THAN
             else:
                 yield c
 
     @staticmethod
     def _pad_gen(num):
-        """Yield the specified number of SPACE characters.
-        """
+        """Yield the specified number of SPACE characters."""
         while num > 0:
-            yield ' '
+            yield " "
             num -= 1
 
     def html(self, indent=0):
@@ -126,34 +129,33 @@ class HTMLElement:
         """
         # Yield the start tag.
         yield from self._pad_gen(indent)
-        yield '<'
+        yield "<"
         yield from self.TAG_NAME
         if self.attrs:
             for k, v in self.attrs.items():
                 if v is None:
                     continue
-                yield ' '
+                yield " "
                 yield from self.encode_attr_key(k)
-                yield '='
+                yield "="
                 yield '"'
                 yield from self.encode_attr_value(v)
                 yield '"'
-        yield '>'
-        yield '\n'
+        yield ">"
+        yield "\n"
 
         # Yield the text.
         if self.text:
-            text = (self.escape_text(self.text) if self.ESCAPE_TEXT
-                    else self.text)
+            text = self.escape_text(self.text) if self.ESCAPE_TEXT else self.text
             if not self.INDENT_TEXT:
                 yield from text
             else:
                 yield from self._pad_gen(indent + self.CHILD_INDENT)
                 for c in text:
                     yield c
-                    if c == '\n':
+                    if c == "\n":
                         yield from self._pad_gen(indent + self.CHILD_INDENT)
-            yield '\n'
+            yield "\n"
 
         # Yield the children.
         if self.children:
@@ -163,178 +165,228 @@ class HTMLElement:
         # Maybe yield closing tag.
         if not self.IS_VOID:
             yield from self._pad_gen(indent)
-            yield '<'
-            yield '/'
+            yield "<"
+            yield "/"
             yield from self.TAG_NAME
-            yield '>'
-            yield '\n'
+            yield ">"
+            yield "\n"
+
 
 # Define an HTMLElemnt subclass from which to derive void elements.
 class VoidHTMLElement(HTMLElement):
     IS_VOID = True
 
+
 # Define a NOEL (No-Element) class that implements a static html() method that
 # allows it to be used wherever a normal element can appear, but which yields
 # only a single empty string. This is useful for inline conditionals, e.g.
 # x = Element(<variable>) if <variable> else NOEL
-class NOEL():
+class NOEL:
     @staticmethod
     def html(indent=None):
-        yield ''
+        yield ""
+
 
 ###############################################################################
 # Common Element Subclasses
 ###############################################################################
 
+
 class Anchor(HTMLElement):
-    TAG_NAME = 'a'
-    REQUIRED_ATTRS = ('href',)
+    TAG_NAME = "a"
+    REQUIRED_ATTRS = ("href",)
+
 
 class Body(HTMLElement):
-    TAG_NAME = 'body'
+    TAG_NAME = "body"
+
 
 class Br(VoidHTMLElement):
-    TAG_NAME = 'br'
+    TAG_NAME = "br"
+
 
 class Button(HTMLElement):
-    TAG_NAME = 'button'
+    TAG_NAME = "button"
+
 
 class Div(HTMLElement):
-    TAG_NAME = 'div'
+    TAG_NAME = "div"
+
 
 class Em(HTMLElement):
-    TAG_NAME = 'em'
+    TAG_NAME = "em"
+
 
 class Form(HTMLElement):
-    TAG_NAME = 'form'
+    TAG_NAME = "form"
+
 
 class H1(HTMLElement):
-    TAG_NAME = 'h1'
+    TAG_NAME = "h1"
+
 
 class H2(HTMLElement):
-    TAG_NAME = 'h2'
+    TAG_NAME = "h2"
+
 
 class H3(HTMLElement):
-    TAG_NAME = 'h3'
+    TAG_NAME = "h3"
+
 
 class H4(HTMLElement):
-    TAG_NAME = 'h4'
+    TAG_NAME = "h4"
+
 
 class H5(HTMLElement):
-    TAG_NAME = 'h5'
+    TAG_NAME = "h5"
+
 
 class H6(HTMLElement):
-    TAG_NAME = 'h6'
+    TAG_NAME = "h6"
+
 
 class Html(HTMLElement):
-    TAG_NAME = 'html'
-    REQUIRED_ATTRS = ('lang',)
+    TAG_NAME = "html"
+    REQUIRED_ATTRS = ("lang",)
+
 
 class Hr(VoidHTMLElement):
-    TAG_NAME = 'hr'
+    TAG_NAME = "hr"
+
 
 class Head(HTMLElement):
-    TAG_NAME = 'head'
+    TAG_NAME = "head"
+
 
 class Img(VoidHTMLElement):
-    TAG_NAME = 'img'
-    REQUIRED_ATTRS = ('src', 'alt')
+    TAG_NAME = "img"
+    REQUIRED_ATTRS = ("src", "alt")
+
 
 class Input(VoidHTMLElement):
-    TAG_NAME = 'input'
-    REQUIRED_ATTRS = ('type',)
+    TAG_NAME = "input"
+    REQUIRED_ATTRS = ("type",)
+
 
 class Label(HTMLElement):
-    TAG_NAME = 'label'
-    REQUIRED_ATTRS = ('_for',)
+    TAG_NAME = "label"
+    REQUIRED_ATTRS = ("_for",)
+
 
 class Li(HTMLElement):
-    TAG_NAME = 'li'
+    TAG_NAME = "li"
+
 
 class Link(VoidHTMLElement):
-    TAG_NAME = 'link'
-    REQUIRED_ATTRS = ('href', 'rel')
+    TAG_NAME = "link"
+    REQUIRED_ATTRS = ("href", "rel")
+
 
 class Meta(VoidHTMLElement):
-    TAG_NAME = 'meta'
+    TAG_NAME = "meta"
+
 
 class Nav(HTMLElement):
-    TAG_NAME = 'nav'
+    TAG_NAME = "nav"
+
 
 class Ol(HTMLElement):
-    TAG_NAME = 'ol'
+    TAG_NAME = "ol"
+
 
 class Option(HTMLElement):
-    TAG_NAME = 'option'
-    REQUIRED_ATTRS = ('value',)
+    TAG_NAME = "option"
+    REQUIRED_ATTRS = ("value",)
+
 
 class Paragraph(HTMLElement):
-    TAG_NAME = 'p'
+    TAG_NAME = "p"
+
 
 class Picture(HTMLElement):
-    TAG_NAME = 'picture'
+    TAG_NAME = "picture"
+
 
 class PictureSource(VoidHTMLElement):
-    TAG_NAME = 'source'
-    REQUIRED_ATTRS = ('srcset',)
+    TAG_NAME = "source"
+    REQUIRED_ATTRS = ("srcset",)
+
 
 class Script(HTMLElement):
-    TAG_NAME = 'script'
+    TAG_NAME = "script"
     ESCAPE_TEXT = False
+
 
 class Section(HTMLElement):
-    TAG_NAME = 'section'
+    TAG_NAME = "section"
+
 
 class Select(HTMLElement):
-    TAG_NAME = 'select'
+    TAG_NAME = "select"
+
 
 class Span(HTMLElement):
-    TAG_NAME = 'span'
+    TAG_NAME = "span"
+
 
 class Strong(HTMLElement):
-    TAG_NAME = 'strong'
+    TAG_NAME = "strong"
+
 
 class Style(HTMLElement):
-    TAG_NAME = 'style'
+    TAG_NAME = "style"
     ESCAPE_TEXT = False
 
+
 class Table(HTMLElement):
-    TAG_NAME = 'table'
+    TAG_NAME = "table"
+
 
 class Tbody(HTMLElement):
-    TAG_NAME = 'tbody'
+    TAG_NAME = "tbody"
+
 
 class Td(HTMLElement):
-    TAG_NAME = 'td'
+    TAG_NAME = "td"
+
 
 class Template(HTMLElement):
-    TAG_NAME = 'template'
+    TAG_NAME = "template"
+
 
 class Textarea(HTMLElement):
-    TAG_NAME = 'textarea'
+    TAG_NAME = "textarea"
     INDENT_TEXT = False
 
+
 class Th(HTMLElement):
-    TAG_NAME = 'th'
+    TAG_NAME = "th"
+
 
 class Thead(HTMLElement):
-    TAG_NAME = 'thead'
+    TAG_NAME = "thead"
+
 
 class Title(HTMLElement):
-    TAG_NAME = 'title'
+    TAG_NAME = "title"
+
 
 class Tr(HTMLElement):
-    TAG_NAME = 'tr'
+    TAG_NAME = "tr"
+
 
 class Ul(HTMLElement):
-    TAG_NAME = 'ul'
+    TAG_NAME = "ul"
+
 
 class Video(HTMLElement):
-    TAG_NAME = 'video'
+    TAG_NAME = "video"
+
 
 class VideoSource(VoidHTMLElement):
-    TAG_NAME = 'source'
-    REQUIRED_ATTRS = ('src',)
+    TAG_NAME = "source"
+    REQUIRED_ATTRS = ("src",)
+
 
 ###############################################################################
 # <meta> helpers
@@ -344,7 +396,7 @@ class VideoSource(VoidHTMLElement):
 StdMeta = lambda k, v: Meta(name=k, content=v)
 
 # OpenGraph: https://ogp.me/
-OGMeta = lambda k, v: Meta(property=f'og:{k}', content=v)
+OGMeta = lambda k, v: Meta(property=f"og:{k}", content=v)
 
 # Microdata meta helper.
 MDMeta = lambda k, v: Meta(itemprop=k, content=v)
@@ -353,11 +405,13 @@ MDMeta = lambda k, v: Meta(itemprop=k, content=v)
 # GenReader Class
 ###############################################################################
 
+
 class GenReader:
     """
     A file-like character generator wrapper/encoder that implements readinto().
     """
-    def __init__(self, gen, encoding='utf-8'):
+
+    def __init__(self, gen, encoding="utf-8"):
         self.gen = gen
         self.encoding = encoding
 
@@ -376,9 +430,11 @@ class GenReader:
                 i += 1
         return i
 
+
 ###############################################################################
 # Document Function
 ###############################################################################
+
 
 def Document(body_els, head_els=()):
     """
@@ -387,21 +443,21 @@ def Document(body_els, head_els=()):
     """
     yield from DOCTYPE
     yield from Html(
-        lang='en',
+        lang="en",
         children=(
             Head(
                 children=(
-                    Meta(charset='utf-8'),
+                    Meta(charset="utf-8"),
                     Meta(
-                        name='viewport',
-                        content='width=device-width, initial-scale=1'
+                        name="viewport", content="width=device-width, initial-scale=1"
                     ),
-                    *head_els
+                    *head_els,
                 )
             ),
-            Body(children=body_els)
-        )
+            Body(children=body_els),
+        ),
     ).html()
+
 
 # Define a GenReader-wrapped version of Document.
 DocumentStream = lambda *args, **kwargs: GenReader(Document(*args, **kwargs))
--- test.py
+++ test.py
@@ -29,156 +29,211 @@ from __init__ import (
 # Helpers
 ###############################################################################
 
-_join = ''.join
+_join = "".join
 assertYields = lambda ins, expected: assertEqual(_join(ins.html()), expected)
 
 ###############################################################################
 # Misc Tests
 ###############################################################################
 
+
 def test_python_version():
     from sys import version_info
+
     major, minor = version_info[:2]
-    if  major < 3 or minor < 6:
+    if major < 3 or minor < 6:
         raise AssertionError(
-            'Tests require Python >= 3.6 for its kwargs order preservation, '
-            f'you\'re using: {major}.{minor}'
+            "Tests require Python >= 3.6 for its kwargs order preservation, "
+            f"you're using: {major}.{minor}"
         )
+
 
 ###############################################################################
 # Encoding / Escaping Method Tests
 ###############################################################################
 
+
 def test_encode_attr_key_no_underscores():
-    assertEqual(_join(HTMLElement.encode_attr_key('testkey')), 'testkey')
+    assertEqual(_join(HTMLElement.encode_attr_key("testkey")), "testkey")
+
 
 def test_encode_attr_key_escaped():
-    assertEqual(_join(HTMLElement.encode_attr_key('__test_key')), '-test-key')
+    assertEqual(_join(HTMLElement.encode_attr_key("__test_key")), "-test-key")
+
 
 def test_encode_attr_key_not_escaped():
-    assertEqual(_join(HTMLElement.encode_attr_key('-_test_key')), '-_test_key')
+    assertEqual(_join(HTMLElement.encode_attr_key("-_test_key")), "-_test_key")
+
 
 def test_encode_attr_value_with_double_quotes():
-    assertEqual(_join(HTMLElement.encode_attr_value('"x"')), '&quot;x&quot;')
+    assertEqual(_join(HTMLElement.encode_attr_value('"x"')), "&quot;x&quot;")
+
 
 def test_escape_text():
-    assertEqual(_join(HTMLElement.escape_text('<html>')), '&lt;html&gt;')
+    assertEqual(_join(HTMLElement.escape_text("<html>")), "&lt;html&gt;")
+
 
 ###############################################################################
 # Element Attribute Tests
 ###############################################################################
 
+
 def test_missing_required_attrs():
     assertRaises(AssertionError, Anchor)
 
+
 def test_present_required_attrs():
-    assertYields(Anchor(href='http://example.com'),
-"""<a href="http://example.com">
+    assertYields(
+        Anchor(href="http://example.com"),
+        """<a href="http://example.com">
 </a>
-""")
+""",
+    )
+
 
 def test_multiple_attrs():
-    assertYields(Anchor(href='http://example.com', id='some-id'),
-"""<a href="http://example.com" id="some-id">
+    assertYields(
+        Anchor(href="http://example.com", id="some-id"),
+        """<a href="http://example.com" id="some-id">
 </a>
-""")
+""",
+    )
+
 
 def test_attr_key_with_leading_underscore():
-    assertYields(Div(_class="container"),
-"""<div class="container">
+    assertYields(
+        Div(_class="container"),
+        """<div class="container">
 </div>
-""")
+""",
+    )
+
 
 def test_attr_key_with_leading_and_subsequent_underscore():
-    assertYields(Div(_data_count=1),
-"""<div data-count="1">
+    assertYields(
+        Div(_data_count=1),
+        """<div data-count="1">
 </div>
-""")
+""",
+    )
+
 
 def test_escaped_attr_value():
-    assertYields(Anchor(href='http://"example".com'),
-"""<a href="http://&quot;example&quot;.com">
+    assertYields(
+        Anchor(href='http://"example".com'),
+        """<a href="http://&quot;example&quot;.com">
 </a>
-""")
+""",
+    )
+
 
 ###############################################################################
 # Element Text Tests
 ###############################################################################
 
+
 def test_nonvoid_no_text():
-    assertYields(Anchor(href='http://example.com'),
-"""<a href="http://example.com">
+    assertYields(
+        Anchor(href="http://example.com"),
+        """<a href="http://example.com">
 </a>
-""")
+""",
+    )
+
 
 def test_nonvoid_with_text():
-    assertYields(Anchor('sample text', href='http://example.com'),
-"""<a href="http://example.com">
+    assertYields(
+        Anchor("sample text", href="http://example.com"),
+        """<a href="http://example.com">
   sample text
 </a>
-""")
+""",
+    )
+
 
 def test_nonvoid_with_escaped_text():
-    assertYields(Anchor('sample <text>', href='http://example.com'),
-"""<a href="http://example.com">
+    assertYields(
+        Anchor("sample <text>", href="http://example.com"),
+        """<a href="http://example.com">
   sample &lt;text&gt;
 </a>
-""")
+""",
+    )
+
 
 def test_void_no_text():
-    assertYields(Br(),
-"""<br>
-""")
+    assertYields(
+        Br(),
+        """<br>
+""",
+    )
+
 
 def test_void_with_text():
-    assertRaises(AssertionError, Br, 'abcd')
+    assertRaises(AssertionError, Br, "abcd")
+
 
 def test_no_text_indent():
-    assertYields(Textarea('abcd'),
-"""<textarea>
+    assertYields(
+        Textarea("abcd"),
+        """<textarea>
 abcd
 </textarea>
-""")
+""",
+    )
+
 
 def test_script_text_is_not_escaped():
     # Expect '<' and '>' to not be escaped.
     assertYields(
         Script('console.log("<test>")'),
-"""<script>
+        """<script>
   console.log("<test>")
 </script>
-""")
+""",
+    )
+
 
 def test_style_text_is_not_escaped():
     # Expect '>' to not be escaped.
     assertYields(
-        Style('body > div { font-family: arial; }'),
-"""<style>
+        Style("body > div { font-family: arial; }"),
+        """<style>
   body > div { font-family: arial; }
 </style>
-""")
+""",
+    )
+
 
 ###############################################################################
 # Element Children Tests
 ###############################################################################
 
+
 def test_void_children_is_none():
     assertNone(Br().children)
 
+
 def test_single_child():
-    assertYields(Div(children=(Span(),)),
-"""<div>
+    assertYields(
+        Div(children=(Span(),)),
+        """<div>
   <span>
   </span>
 </div>
-""")
+""",
+    )
+
 
 def test_lots_of_complicated_kids():
-    assertYields(Div(children=[
-        Span(f'{i}', id=i, _class=f'style-{i}', children=(Span(f'{i + 1}'),))
-        for i in range(3)
-    ]),
-"""<div>
+    assertYields(
+        Div(
+            children=[
+                Span(f"{i}", id=i, _class=f"style-{i}", children=(Span(f"{i + 1}"),))
+                for i in range(3)
+            ]
+        ),
+        """<div>
   <span id="0" class="style-0">
     0
     <span>
@@ -198,14 +253,17 @@ def test_lots_of_complicated_kids():
     </span>
   </span>
 </div>
-""")
+""",
+    )
+
 
 def test_appending_children():
     el = Div()
-    el.children.append(Span('0'))
-    el.children[0].children.append(Span('1'))
-    assertYields(el,
-"""<div>
+    el.children.append(Span("0"))
+    el.children[0].children.append(Span("1"))
+    assertYields(
+        el,
+        """<div>
   <span>
     0
     <span>
@@ -213,29 +271,34 @@ def test_appending_children():
     </span>
   </span>
 </div>
-""")
+""",
+    )
+
 
 def test_a_from_scratch_document():
     assertYields(
-        Html(lang='en', children=[
-            Head(children=[
-                Meta(charset='utf-8'),
-                Title('a <good> page'),
-                Script('console.log("<hello>")'),
-                Style(
-"""body {
+        Html(
+            lang="en",
+            children=[
+                Head(
+                    children=[
+                        Meta(charset="utf-8"),
+                        Title("a <good> page"),
+                        Script('console.log("<hello>")'),
+                        Style(
+                            """body {
   font-family: arial;
 }
 body > div {
   padding: 8px;
 }"""
-                )
-            ]),
-            Body(children=[
-                Div('some text', id="id-1", _class="class-1")
-            ])
-        ]),
-"""<html lang="en">
+                        ),
+                    ]
+                ),
+                Body(children=[Div("some text", id="id-1", _class="class-1")]),
+            ],
+        ),
+        """<html lang="en">
   <head>
     <meta charset="utf-8">
     <title>
@@ -259,15 +322,19 @@ body > div {
     </div>
   </body>
 </html>
-""")
+""",
+    )
+
 
 ###############################################################################
 # Document Tests
 ###############################################################################
 
+
 def test_empty_document():
-    assertEqual(''.join(Document(())),
-"""<!DOCTYPE html>
+    assertEqual(
+        "".join(Document(())),
+        """<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -276,14 +343,18 @@ def test_empty_document():
   <body>
   </body>
 </html>
-""")
+""",
+    )
+
 
 def test_nonempty_document():
-    assertEqual(''.join(Document(
-        body_els=(Span('abcd'),),
-        head_els=(Script('console.log("hello")'),)
-    )),
-"""<!DOCTYPE html>
+    assertEqual(
+        "".join(
+            Document(
+                body_els=(Span("abcd"),), head_els=(Script('console.log("hello")'),)
+            )
+        ),
+        """<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -298,24 +369,27 @@ def test_nonempty_document():
     </span>
   </body>
 </html>
-""")
+""",
+    )
 
 
 ###############################################################################
 # GenReader Tests
 ###############################################################################
 
+
 def test_genreader_with_nonascii_chars():
-    expected = \
-"""<div>
+    expected = """<div>
   ĀāĂăĄą
 </div>
-""".encode('utf-8')
-    reader = GenReader(Div('ĀāĂăĄą').html(), encoding='utf-8')
+""".encode(
+        "utf-8"
+    )
+    reader = GenReader(Div("ĀāĂăĄą").html(), encoding="utf-8")
     buf = bytearray(len(expected))
     reader.readinto(buf)
     assertEqual(buf, expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli(globals())
